659d57874c77032de07b45a28d7a0e83f7d170ed
Logan <10239411+Loag@users.noreply.github.com>
2025-03-12 12:53:29 -0400

    use bool instead of uint

--- pkg/bloomfilter/bloomfilter.go
+++ pkg/bloomfilter/bloomfilter.go
@@ -4,34 +4,34 @@ import "github.com/Loag/FNV-1a/pkg/fnv1a_fast"
 
 type BloomFilter struct {
 	size  uint32
-	items []uint32
+	items []bool
 }
 
 func NewBloomFilter(size uint32) *BloomFilter {
-	items := make([]uint32, size)
+	items := make([]bool, size)
 	return &BloomFilter{size: size, items: items}
 }
 
 func (h *BloomFilter) Add(item []byte) {
 	val := fnv1a_fast.FNV1a32(item)
 	index := val % h.size
-	h.items[index] = val
+	h.items[index] = true
 }
 
 func (h *BloomFilter) Contains(item []byte) bool {
 	val := fnv1a_fast.FNV1a32(item)
 	index := val % h.size
-	return h.items[index] == val
+	return h.items[index] == true
 }
 
 func (h *BloomFilter) Remove(item []byte) {
 	val := fnv1a_fast.FNV1a32(item)
 	index := val % h.size
-	h.items[index] = 0
+	h.items[index] = false
 }
 
 func (h *BloomFilter) RemoveAll() {
 	for i := range h.items {
-		h.items[i] = 0
+		h.items[i] = false
 	}
 }
